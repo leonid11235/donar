c87fb53c2de2ee590acc287550bb5bddff2a3592
kev-th <kevinkthaw@gmail.com>
2018-04-28 01:49:23 -0400

    add views

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 //Import Express
 const express = require('express');
 const app = express();
+const router = express.Router();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 
+//app.use(express.static(__dirname, '/views'));
+
+var html_dir = './views/'
 //Start the server
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
@@ -12,4 +17,8 @@ app.listen(port, () => {
 //Rout to the index/home page
 app.get('/', (req,res)=>{
     res.send("Hello");
+})
+
+app.get('/main', (req,res)=>{
+    res.sendfile(html_dir +'test.html');
 })
